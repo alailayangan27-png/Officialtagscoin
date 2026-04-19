dd18166099b53684e9ee01788f78a07f6f67781c
alailayangan27-png <alailayangan27@gmail.com>
2026-04-20 00:27:47 +0700

    Refactor CONFIG object formatting in config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-export const CONFIG = {
-  RECEIVER: "UQAPRU6cHYSkS8hIxl-zbcts9yt8_GtYcSh_R0nbYnWL5lFX",
-  MIN: 1,
-  MAX: 100,
-  RATE: 10000
-};
+export const CONFIG={
+RECEIVER:"UQAPRU6cHYSkS8hIxl-zbcts9yt8_GtYcSh_R0nbYnWL5lFX",
+MIN:1,
+MAX:100,
+RATE:10000
+}
